2e20f9a914f7be3bf0ca64bc114857e477e9eacd
Nick <nick.ge@astasys.com>
2019-04-10 09:31:11 +0800

    Revert "change now"
    This reverts commit f9de3db536caf260123f3c828ea8a7adea024b3a.

--- helloKoa2/routes/index.js
+++ helloKoa2/routes/index.js
@@ -13,6 +13,10 @@ router.get('/student', async (ctx, next) => {
   ctx.body = 'git test'
 })
 
-
+router.get('/json', async (ctx, next) => {
+  ctx.body = {
+    title: 'koa2 json'
+  }
+})
 
 module.exports = router
